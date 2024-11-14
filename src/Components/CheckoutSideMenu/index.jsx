d9c17d776a31f250fe49b2a70fe71cb759bd24e7
Fabian Velasquez <fabianvegadev@gmail.com>
2024-11-14 07:29:48 -0500

    Clases 15 a 20 (Carrito de compras)

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,51 +1,58 @@
-import { useContext } from 'react'
-import { XMarkIcon } from '@heroicons/react/24/solid'
-import { ShoppingCartContext } from '../../Context'
-import OrderCard from '../../Components/OrderCard'
-import { totalPrice } from '../../utils'
-import './styles.css'
+import { useContext } from "react";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import { ShoppingCartContext } from "../../Context";
+import OrderCard from "../../Components/OrderCard";
+import { totalPrice } from "../../utils";
+import "./styles.css";
 
 const CheckoutSideMenu = () => {
-  const context = useContext(ShoppingCartContext)
+	const context = useContext(ShoppingCartContext);
 
-  const handleDelete = (id) => {
-    const filteredProducts = context.cartProducts.filter(product => product.id != id)
-    context.setCartProducts(filteredProducts)
-  }
+	const handleDelete = (id) => {
+		const filteredProducts = context.cartProducts.filter(
+			(product) => product.id != id
+		);
+		context.setCartProducts(filteredProducts);
+		context.setCount(context.count - 1);
+	};
 
-  return (
-    <aside
-      className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
-      <div className='flex justify-between items-center p-6'>
-        <h2 className='font-medium text-xl'>My Order</h2>
-        <div>
-          <XMarkIcon
-            className='h-6 w-6 text-black cursor-pointer'
-            onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
-        </div>
-      </div>
-      <div className='px-6 overflow-y-scroll'>
-        {
-          context.cartProducts.map(product => (
-            <OrderCard
-              key={product.id}
-              id={product.id}
-              title={product.title}
-              imageUrl={product.image}
-              price={product.price}
-              handleDelete={handleDelete}
-            />
-          ))
-        }
-      </div>
-      <div className='px-6'>
-        <p className='flex justify-between items-center'>
-          <span className='font-light'>Total:</span>
-          <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
-        </p>
-      </div>
-    </aside>
-  )
-}
+	return (
+		<aside
+			className={`${
+				context.isCheckoutSideMenuOpen ? "flex" : "hidden"
+			} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}
+		>
+			<div className="flex justify-between items-center p-6">
+				<h2 className="font-medium text-xl">My Order</h2>
+				<div>
+					<XMarkIcon
+						className="h-6 w-6 text-black cursor-pointer"
+						onClick={() => context.closeCheckoutSideMenu()}
+					></XMarkIcon>
+				</div>
+			</div>
+			<div className="px-6 overflow-y-scroll">
+				{context.cartProducts.map((product) => (
+					<OrderCard
+						key={product.id}
+						id={product.id}
+						title={product.title}
+						imageUrl={product.image}
+						price={product.price}
+						handleDelete={handleDelete}
+					/>
+				))}
+			</div>
+			<div className="px-6">
+				<p className="flex justify-between items-center">
+					<span className="font-light">Total:</span>
+					<span className="font-medium text-2xl">
+						${totalPrice(context.cartProducts)}
+					</span>
+				</p>
+			</div>
+		</aside>
+	);
+};
 
-export default CheckoutSideMenu
+export default CheckoutSideMenu;
